4248ad1474161c54b3dd4cde53c32649c3b0af96
Barath Kurapati <barathkurapati@unm.edu>
2023-12-07 18:26:56 +0000

    Replace ClientHandler.java

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,80 +1,87 @@
 import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.List;
 
-public class ClientHandler implements Runnable{
-    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
-    private Socket socket;
-    private BufferedReader bufferedreader;
+public class ClientHandler implements Runnable {
+    public static final List<ClientHandler> clientHandlers = new ArrayList<>();
+    private  Socket socket;
+    private  BufferedReader bufferedreader;
     private BufferedWriter bufferedwriter;
+    private  String clientUsername;
 
-    private String clientUsername;
-
-    public ClientHandler(Socket socket){
-        try{
+    public ClientHandler(Socket socket) {
+        try {
             this.socket = socket;
             this.bufferedwriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedreader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.clientUsername = bufferedreader.readLine();
-            clientHandlers.add(this);
-            broadcastMessage("Server", clientUsername + " has joined the chat");
-        } catch(IOException e) {
-            closeEverything(socket, bufferedreader, bufferedwriter);
+            synchronized (clientHandlers) {
+                clientHandlers.add(this);
+            }
+            broadcastMessage(clientUsername + " has joined the chat");
+        } catch (IOException e) {
+            closeEverything();
+            e.printStackTrace();
         }
     }
-
-
-
 
     @Override
     public void run() {
         String messageFromClient;
-        while (socket.isConnected()){
-            try{
+        while (socket.isConnected()) {
+            try {
                 messageFromClient = bufferedreader.readLine();
-                broadcastMessage(messageFromClient, clientUsername + " has left the chat");
+                if (messageFromClient == null) {
+                    break;
+                }
+                broadcastMessage(clientUsername + ": " + messageFromClient);
             } catch (IOException e) {
-                closeEverything(socket, bufferedreader, bufferedwriter);
+                closeEverything();
                 break;
             }
         }
+        closeEverything();
     }
 
-    public void broadcastMessage(String messageToSend, String s){
-        for (ClientHandler clientHandler : clientHandlers){
-            try{
-                if (!clientHandler.clientUsername.equals(this.clientUsername)){
-                    clientHandler.bufferedwriter.write(messageToSend);
-                    clientHandler.bufferedwriter.newLine();
-                    clientHandler.bufferedwriter.flush();
+    public void broadcastMessage(String messageToSend) {
+        synchronized (clientHandlers) {
+            for (ClientHandler clientHandler : clientHandlers) {
+                try {
+                    if (!clientHandler.clientUsername.equals(clientUsername)) {
+                        clientHandler.bufferedwriter.write(messageToSend);
+                        clientHandler.bufferedwriter.newLine();
+                        clientHandler.bufferedwriter.flush();
+                    }
+                } catch (IOException e) {
+                    closeEverything();
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                closeEverything(socket, bufferedreader, bufferedwriter);
             }
         }
     }
 
-    public void removeClientHandler(){
-        clientHandlers.remove(this);
-        broadcastMessage("Server", clientUsername + " has left the chat");
+    public void removeClientHandler() {
+        synchronized (clientHandlers) {
+            clientHandlers.remove(this);
+        }
+        broadcastMessage(clientUsername + " has left the chat");
     }
 
-    public void closeEverything(Socket socket, BufferedReader bufferedreader, BufferedWriter bufferedwriter){
+    public void closeEverything() {
         removeClientHandler();
-        try{
-            if (socket != null){
+        try {
+            if (socket != null) {
                 socket.close();
             }
-            if (bufferedreader != null){
+            if (bufferedreader != null) {
                 bufferedreader.close();
             }
-            if (bufferedwriter != null){
+            if (bufferedwriter != null) {
                 bufferedwriter.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-
 }
